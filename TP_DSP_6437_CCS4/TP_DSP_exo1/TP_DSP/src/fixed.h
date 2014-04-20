fc0779411a0416e05dd6430b9cdeba302ec3610c
LE STUM Sébastien <sebastien.lestum@edu.esiee.fr>
2014-04-20 16:37:03 +0200

    Adding fixed point computation

---
+++ TP_DSP_6437_CCS4/TP_DSP_exo1/TP_DSP/src/fixed.h
@@ -0,0 +1,17 @@
+#ifndef FIXED_H_
+#define FIXED_H_
+
+#include <stdio.h>
+typedef int fixed;
+
+#define FRACT_BITS 16
+#define FRACT_BITS_D2 8
+
+#define FIXED_ONE (1 << FRACT_BITS)
+#define INT2FIXED(x) ((x) << FRACT_BITS)
+#define FLOAT2FIXED(x) ((int)((x) * (1 << FRACT_BITS))) 
+#define FIXED2INT(x) ((x) >> FRACT_BITS)
+#define FIXED2DOUBLE(x) (((double)(x)) / (1 << FRACT_BITS))
+#define MULT(x, y) ( ((x) >> FRACT_BITS_D2) * ((y)>> FRACT_BITS_D2) )
+
+#endif /*FIXED_H_*/
