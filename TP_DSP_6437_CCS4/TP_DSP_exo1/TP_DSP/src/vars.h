471459377439be743833c9c4275a2447a5cb104d
LE STUM Sébastien <sebastien.lestum@edu.esiee.fr>
2014-04-20 16:28:46 +0200

    Factoring global variables

---
+++ TP_DSP_6437_CCS4/TP_DSP_exo1/TP_DSP/src/vars.h
@@ -0,0 +1,24 @@
+#ifndef VARS_H_
+#define VARS_H_
+
+/* IMAGE RELATED CONSTANTS */
+#define WIDTH 720
+#define HEIGHT 480
+#define NB_PIXELS WIDTH*HEIGHT
+#define ITERNUM 10000
+
+/* MISC */
+#define GAMMA 0.20
+#define SEUIL 130
+#define MAX_VOTES 10
+
+/* MATHS CONSTANTS */
+#define MAXTHETA 256 
+#define PI 3.14159265 
+#define I_PI 0.31830989
+#define SQRT_MAGIC_D 0x5fe6ec85e7de30da
+
+/* VERSION FLAG */
+#define OPTIMISE 1
+
+#endif /*VARS_H_*/
